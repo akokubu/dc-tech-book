9f22ec421908681f94f444268725f9abf1c0be3a
Atsushi Kokubu <akokubu@gmail.com>
2020-06-24 14:16:56 +0900

    userモデル

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,13 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
   layout false
-  def new
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    # if user && user.authenticate(params[:session][:password])
+    render 'new'
   end
 end
