bcc27e07873d7cdea8efad1d9ea19acb046e6621
Atsushi Kokubu <akokubu@gmail.com>
2020-06-24 17:27:55 +0900

    ユーザ登録

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'logout', to: 'sessions#desgroy'
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
+  resources :users
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
